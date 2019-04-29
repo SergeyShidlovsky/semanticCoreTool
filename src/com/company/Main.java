1301cfe6b846af1a7f9aea11d42004508b4dbdf1
SergeyShidlovsky <shedlovskys@gmail.com>
2019-04-29 17:04:06 +0300

    Final commit on this day

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        TextScanner ts = new TextScanner();
+        String inputData = ts.readFile();
+        TextBreaker tb = new TextBreaker();
+        List<String> output = TextBreaker.getWords(inputData);
+        System.out.println("Sorted text: " + output);
     }
 }
--- src/com/company/TextBreaker.java
+++ src/com/company/TextBreaker.java
@@ -1,4 +1,27 @@
 package com.company;
 
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextBreaker {
+
+    // Based on Solution
+    // http://qaru.site/questions/179094/converting-a-sentence-string-to-a-string-array-of-words-in-java
+
+    public static List<String> getWords(String text) {
+        List<String> words = new ArrayList<String>();
+        BreakIterator breakIterator = BreakIterator.getWordInstance();
+        breakIterator.setText(text);
+        int lastIndex = breakIterator.first();
+        while (BreakIterator.DONE != lastIndex) {
+            int firstIndex = lastIndex;
+            lastIndex = breakIterator.next();
+            if (lastIndex != BreakIterator.DONE && Character.isLetterOrDigit(text.charAt(firstIndex))) {
+                words.add(text.substring(firstIndex, lastIndex));
+            }
+        }
+
+        return words;
+    }
 }
