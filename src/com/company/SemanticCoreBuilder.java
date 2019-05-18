42d7b9e59dc33fa1b76ba5fea21b64a5da976803
Sergey Shidlovsky <sergey.shydlovsky@just-eat.com>
2019-05-18 13:26:31 +0300

    added Result UI with dispayed semantic core

--- src/com/company/SemanticCoreBuilder.java
+++ src/com/company/SemanticCoreBuilder.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
 import javax.management.ObjectName;
+import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,10 +16,20 @@ public class SemanticCoreBuilder {
     private float percentage;
     private float classicToshnota;
     private String maximalKeyOfWord;
-
+    BufferedWriter result;
+    FileOutputStream fos;
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     Map <String, Integer> sortedKeyValueData = new TreeMap<>();
 
+    public void initializeResultFile(){
+        try {
+            //result = new FileWriter("../../semanticCoreTool/result.txt");
+            result = new BufferedWriter(new FileWriter("../../semanticCoreTool/result.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int getQuantityOfUniqueWords(Map keyValueData){
         sortedKeyValueData.putAll(keyValueData);
@@ -33,8 +47,7 @@ public class SemanticCoreBuilder {
         quantityOfwords = getQuantityOfUniqueWords(keyValueData);
         maximalValueOfWord=0;
         buffer = 0;
-
-
+        initializeResultFile();
 
         for (int i = 0; i <keyValueData.size() ; i++) {
 
@@ -54,14 +67,30 @@ public class SemanticCoreBuilder {
             percentage = ((float) Integer.valueOf(keyValueData.
                     get(keyValueData.keySet().
                             toArray()[i]).toString()) /  quantityOfwords) * 100;
+
 //            System.out.println("Values collection from map - " +
 //                    keyValueData.keySet().toArray()[i]  + " - " +
 //                    keyValueData.get(keyValueData.keySet().toArray()[i]) + " - " +
 //                    percentage + " %"
 //            );
-
+            try {
+                result.write(sortedKeyValueData.keySet().toArray()[i].toString());
+                result.write( " - ");
+                result.write(sortedKeyValueData.get(sortedKeyValueData.keySet().toArray()[i]).toString());
+                result.write( " - ");
+                result.write(df.format(percentage));
+                result.write( " %");
+                result.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             System.out.println( sortedKeyValueData.keySet().toArray()[i]  + " - " + sortedKeyValueData.get(sortedKeyValueData.keySet().toArray()[i]));
          }
+        try {
+            result.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println( "Word \"" + maximalKeyOfWord + "\" has maximal value " +
                 maximalValueOfWord);
     }
